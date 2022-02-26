bf4456e0f06b8792c7171a1f5169128b673005ae
ed-dickinson <edwardejd@gmail.com>
2022-02-26 11:50:58 +0000

    win/lose condition

--- game.js
+++ game.js
@@ -1,30 +1,37 @@
-let target_word = valid_answers[Math.floor(Math.random()*valid_answers.length)]
-console.log(target_word)
-let target_word_array = target_word.split('');
+let target_word, target_word_array, all_letters;
 
-let common_extra_letters = 'ear';
-let less_common_extra_letters = 'otlis';
-let extra_letter_array = common_extra_letters.split('');
-less_common_extra_letters.split('').forEach(letter => {
-  extra_letter_array.push(letter);
-})
+let goes = 0;
 
-const extra_letters_to_add = 3;
+const setUp = () => {
+  target_word = valid_answers[Math.floor(Math.random()*valid_answers.length)]
+  console.log(target_word)
+  target_word_array = target_word.split('');
 
-let all_letters = target_word.split('');
+  let common_extra_letters = 'ear';
+  let less_common_extra_letters = 'otlis';
+  let extra_letter_array = common_extra_letters.split('');
+  less_common_extra_letters.split('').forEach(letter => {
+    extra_letter_array.push(letter);
+  })
 
-while (all_letters.length < 8) {
-  let letter = extra_letter_array.shift();
-  if (!target_word.includes(letter)) {
-    all_letters.push(letter);
+  const extra_letters_to_add = 3;
+
+  all_letters = target_word.split('');
+
+  while (all_letters.length < 8) {
+    let letter = extra_letter_array.shift();
+    if (!target_word.includes(letter)) {
+      all_letters.push(letter);
+    }
   }
 }
+setUp();
 
 console.log(all_letters)
 
-const keyboard_container = document.querySelector('#keyboard');
-
-const keyboard_keys = 'qwertyuiopasdfghjkl>zxcvbnm<'
+// const keyboard_container = document.querySelector('#keyboard');
+//
+// const keyboard_keys = 'qwertyuiopasdfghjkl>zxcvbnm<'
 
 const banner = document.querySelector('#banner');
 const banner_message = document.querySelector('#message');
@@ -72,6 +79,16 @@ const dropLetter = (letter, column) => {
       all_letters.splice(all_letters.indexOf(last_letter), 1)
       console.log(all_letters)
     }
+    // rows[row];
+
+
+    if (Array.from(rows[row].children).every((child)=>{
+      return child.classList.contains('right-position')
+    })) {
+      gameWon();
+    } else if (goes === 30) {
+      gameLost();
+    }
   }
 
   const checkAndDrop = () => {
@@ -94,6 +111,7 @@ const selectColumn = (column) => {
   // console.log(column)
   dropLetter(next_letter, column)
   updateNextLetter();
+  goes++;
 }
 
 for (let i = 0; i < 6; i++) {
@@ -155,121 +173,17 @@ const gameWon = () => {
   console.log('you win!')
   banner.classList.remove('hidden')
   banner_message.innerHTML = 'WIN!';
-  banner_goes.innerHTML = (guess) + ' guess' + (guess > 2 ? 'es' : '');
+  // banner_goes.innerHTML = (guess) + ' guess' + (guess > 2 ? 'es' : '');
 }
 
 const gameLost = () => {
   console.log('you lose...')
   banner.classList.remove('hidden')
   banner_message.innerHTML = 'LOSE...';
-  banner_goes.innerHTML = (guess-1) + ' guess' + (guess > 2 ? 'es' : '');
+  // banner_goes.innerHTML = (guess-1) + ' guess' + (guess > 2 ? 'es' : '');
 }
 
-// const enterPress = () => {
-//   console.log('enter', tile)
-//
-//   if (tile === 6) {
-//     let valid_guess = valid_guesses.indexOf(current_guess) !== -1 ? true : valid_answers.indexOf(current_guess) !== -1 ? true : false;
-//     // let valid_answer = valid_answers.indexOf(current_guess) !== -1 ? true : false;
-//     // valid_guesses.indexOf(current_guess)
-//     console.log(valid_guess)
-//
-//     if (valid_guess) {
-//
-//
-//       let guess_array = current_guess.split('')
-//       let target_word_array = target_word.split('')
-//
-//
-//       for (let i = 0; i < 5; i++) {
-//         if (target_word.includes(current_guess[i])) {
-//
-//
-//           let letter_position = target_word_array.indexOf(current_guess[i])
-//           // if (letter_position === i) {
-//           if (current_guess[i] === target_word[i]) {
-//             rows[guess-1].children[i].classList.add('right-position')
-//           } else {
-//             rows[guess-1].children[i].classList.add('wrong-position')
-//           }
-//
-//         } else {
-//           // grey out keyboard
-//           document.querySelector('.letter-' + current_guess[i]).classList.add('key-in-word')
-//         }
-//       }
-//
-//
-//       rows[guess-1].classList.add('submitted');
-//
-//
-//       tile = 1;
-//
-//       if (current_guess === target_word) {
-//         gameWon();
-//         return;
-//       } else if (guess > 5) {
-//         gameLost();
-//       }
-//
-//       guess++;
-//       current_guess = '';
-//
-//     }
-//
-//   }
-// }
 
-// const backspacePress = () => {
-//   // console.log('backspace')
-//
-//   if (tile > 1) {
-//     rows[guess-1].children[tile-2].innerHTML = '';
-//     tile--;
-//     current_guess = current_guess.slice(0,current_guess.length-1)
-//   }
-//
-//   // console.log(current_guess)
-// }
-//
-// for (let i = 0; i < keyboard_keys.length; i++) {
-//   let key = document.createElement('button')
-//   key.classList.add('key');
-//   key.innerHTML = keyboard_keys.charAt(i)
-//
-//   keyboard_container.children[i < 10 ? 0 : i < 19 ? 1 : 2].appendChild(key)
-//
-//   if (i === 19) {
-//     key.classList.add('enter')
-//   } else if (i === 27) {
-//     key.classList.add('backspace')
-//   } else {
-//     key.classList.add('letter-' + keyboard_keys.charAt(i))
-//   }
-//
-//
-//
-//   key.addEventListener('click',()=>{
-//     i === 19 ? enterPress() : i === 27 ? backspacePress() :
-//     keyPress(keyboard_keys.charAt(i))
-//   })
-// }
-//
-// console.log(keyboard_container.clientWidth / window.innerWidth)
-//
-// const window_width = window.innerWidth - 10;
-// const keyboard_width = keyboard_container.clientWidth;
-// const key_window_ratio = (window.innerWidth /keyboard_container.clientWidth) * 0.98
-
-
-
-
-
-// keyboard_container.style.transform = 'scale('+(key_window_ratio)+')'
-// keyboard_container.style.bottom = keyboard_container.clientHeight / key_window_ratio / 10 + 'px'
-// keyboard_container.style.left = window_width / key_window_ratio / 10 + 'px'
-
-// let rows = document.querySelectorAll('row');
 
 
 let guess = 1;
@@ -295,9 +209,23 @@ const replayGame = () => {
   console.log(target_word)
 }
 
-const replay_button = document.querySelector('#replay')
+const resetGame = () => {
+  setUp();
+  // clearInterval(timed_loop);
+  document.querySelectorAll('#tiles .space').forEach(space => {
+    space.innerHTML = '';
+    space.classList.remove('right-position')
+    space.classList.remove('wrong-position')
+    space.classList.remove('wrong-letter')
+  })
+
+  goes = 0;
+}
+
+const replay_button = document.querySelector('#reset')
 
 replay_button.addEventListener('click',()=>{
   // location.reload()
-  replayGame();
+  // replayGame();
+  resetGame();
 })
